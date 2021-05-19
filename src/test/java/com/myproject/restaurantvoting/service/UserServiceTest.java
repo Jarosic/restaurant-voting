ef305cbca0b5a4ccca0e85d1955ebff998a1c631
Rybalka Yaroslav <jaroslav.rybalka@ukr.net>
2021-05-19 16:46:38 +0300

    Fix user service tests

--- src/test/java/com/myproject/restaurantvoting/service/UserServiceTest.java
+++ src/test/java/com/myproject/restaurantvoting/service/UserServiceTest.java
@@ -3,8 +3,7 @@ package com.myproject.restaurantvoting.service;
 import com.myproject.restaurantvoting.data.UserTestData;
 import com.myproject.restaurantvoting.model.User;
 import com.myproject.restaurantvoting.util.exception.VotingTimeLimitException;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
@@ -14,6 +13,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserServiceTest extends AbstractServiceTest {
 
     @Autowired
@@ -21,49 +21,66 @@ public class UserServiceTest extends AbstractServiceTest {
 
     @Test
     @Override
+    @Order(1)
     public void get() {
         User expected = service.get(ID);
         User actual = UserTestData.USER;
         actual.setRegistered(expected.getRegistered());
-        assertThat(actual).usingRecursiveComparison().ignoringFields("registered").isEqualTo(expected);
+        assertThat(actual).usingRecursiveComparison()
+                .ignoringFields("registered")
+                .isEqualTo(expected);
     }
 
     @Test
     @Override
+    @Order(2)
     public void getAll() {
         List<User> expected = service.getAll();
         List<User> actual = UserTestData.getUserList();
-        assertThat(actual).usingElementComparatorIgnoringFields("registered").isEqualTo(expected);
+        assertThat(actual)
+                .usingElementComparatorIgnoringFields("registered")
+                .isEqualTo(expected);
     }
 
     @Test
+    @Order(3)
     public void getByEmail() {
         User expected = service.getByEmail("user@yandex.ru");
         User actual = UserTestData.USER;
-        assertThat(actual).usingRecursiveComparison().ignoringFields("registered").isEqualTo(expected);
+        assertThat(actual).
+                usingRecursiveComparison().
+                ignoringFields("registered").
+                isEqualTo(expected);
     }
 
     @Test
     @Override
+    @Order(4)
     public void create() {
         User newUser = UserTestData.getNew();
         service.create(newUser);
         User expected = service.get(ID + 12);
         newUser.setId(ID + 12);
-        assertThat(newUser).usingRecursiveComparison().ignoringFields("registered").isEqualTo(expected);
+        assertThat(newUser).usingRecursiveComparison()
+                .ignoringFields("registered")
+                .isEqualTo(expected);
     }
 
     @Test
     @Override
+    @Order(5)
     public void update() {
         User update = UserTestData.getUpdate();
         service.update(update, ID);
         User expected = service.get(ID);
-        assertThat(update).usingRecursiveComparison().ignoringFields("registered").isEqualTo(expected);
+        assertThat(update).usingRecursiveComparison()
+                .ignoringFields("registered")
+                .isEqualTo(expected);
     }
 
     @Test
     @Override
+    @Order(7)
     public void delete() {
         Assertions.assertTrue(service.delete(ID));
     }
@@ -75,10 +92,14 @@ public class UserServiceTest extends AbstractServiceTest {
         service.vote(update, ID + 1, null);
         User expected = service.get(ID);
         update.setVotingDateTime(expected.getVotingDateTime());
-        assertThat(update).usingRecursiveComparison().ignoringFields("registered").isEqualTo(expected);
+        assertThat(update)
+                .usingRecursiveComparison()
+                .ignoringFields("registered")
+                .isEqualTo(expected);
     }
 
     @Test
+    @Order(8)
     public void reVoteWithoutException() {
         LocalDateTime firstVote = LocalDateTime.of(LocalDate.now(), LocalTime.of(10, 0));
         LocalDateTime secondVote = LocalDateTime.of(LocalDate.now(), LocalTime.of(11, 0));
@@ -91,10 +112,14 @@ public class UserServiceTest extends AbstractServiceTest {
         service.vote(reVote, ID + 2, secondVote);
 
         User expected = service.get(ID);
-        assertThat(reVote).usingRecursiveComparison().ignoringFields("registered").isEqualTo(expected);
+        assertThat(reVote)
+                .usingRecursiveComparison()
+                .ignoringFields("registered")
+                .isEqualTo(expected);
     }
 
     @Test
+    @Order(9)
     public void reVoteWithException() {
         LocalDateTime firstVote = LocalDateTime.of(LocalDate.now(), LocalTime.of(10, 0));
         LocalDateTime secondVote = LocalDateTime.of(LocalDate.now(), LocalTime.of(13, 0));
