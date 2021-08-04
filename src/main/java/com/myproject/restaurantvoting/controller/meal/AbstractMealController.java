9107a8001495fc1c9702a4168544f43f47f08103
Rybalka Yaroslav <jaroslav.rybalka@ukr.net>
2021-08-04 17:52:33 +0300

    Fix Abstract meal controller

--- src/main/java/com/myproject/restaurantvoting/controller/meal/AbstractMealController.java
+++ src/main/java/com/myproject/restaurantvoting/controller/meal/AbstractMealController.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Slf4j
-public class AbstractMealController {
+public abstract class AbstractMealController {
 
     @Autowired
     protected MealService service;
